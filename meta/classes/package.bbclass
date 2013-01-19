openembedded/openembedded-core

commit b27b438221e16ac3df6ac66d761b77e3bd43db67

prs: use the PRServer to replace the BB_URI_LOCALCOUNT functionality
- adapt the PRServer to generate an incremental number for every revision set
associated with a (PN, PV)
- use one incrementing value rather than several
- PV becomes 0.1+gitAUTOINC+deadbeefdecafbad_decafbaddeadbeef
- if PV contains 'AUTOINC' set PKGV to the same thing with AUTOINC replaced
with the "auto PR" value
- if the PRServer isn't running replace AUTOINC with 0 (no package feeds, but usual
image creation would work)
- use the PR table with "AUTOINC-${PV-without-SRCREVs}" as key
- modify prexport/primport to address the generated srcrev entries
- this patch addresses the following proposal:
http://lists.linuxtogo.org/pipermail/bitbake-devel/2012-November/003878.html

Signed-off-by: Constantin Musca <constantinx.musca@intel.com>
Signed-off-by: Saul Wold <sgw@linux.intel.com>

meta/classes/package.bbclass
@@ -411,6 +411,10 @@ python package_get_auto_pr() {
                 bb.fatal("Can NOT get PRAUTO from remote PR service")
             return
         d.setVar('PRAUTO',str(auto_pr))
+    else:
+        pv = d.getVar("PV", True)
+        if 'AUTOINC' in pv:
+            d.setVar("PKGV", pv.replace("AUTOINC", "0"))
 }
 
 LOCALEBASEPN ??= "${PN}"

meta/classes/prexport.bbclass
@@ -24,6 +24,16 @@ python prexport_handler () {
             bb.warn("prexport_handler: No AUTOPR values found for %s" % ver)
             return
         oe.prservice.prserv_export_tofile(e.data, None, datainfo, False)
+        if 'AUTOINC' in ver:
+            import re
+            srcpv =  bb.fetch2.get_srcrev(e.data)
+            base_ver = "AUTOINC-%s" % ver[:ver.find(srcpv)]
+            e.data.setVar('PRSERV_DUMPOPT_VERSION', base_ver)
+            retval = oe.prservice.prserv_dump_db(e.data)
+            if not retval:
+                bb.fatal("prexport_handler: export failed!")
+            (metainfo, datainfo) = retval
+            oe.prservice.prserv_export_tofile(e.data, None, datainfo, False)
     elif isinstance(e, bb.event.ParseStarted):
         import bb.utils
         import oe.prservice

meta/classes/prserv.bbclass
@@ -1,6 +1,11 @@
 def prserv_get_pr_auto(d):
     import oe.prservice
+    import re
+
+    pv = d.getVar("PV", True)
     if d.getVar('USE_PR_SERV', True) != "1":
+        if 'AUTOINC' in pv:
+            d.setVar("PKGV", pv.replace("AUTOINC", "0"))
         bb.warn("Not using network based PR service")
         return None
 
@@ -8,14 +13,21 @@ def prserv_get_pr_auto(d):
     pkgarch = d.getVar("PACKAGE_ARCH", True)
     checksum = d.getVar("BB_TASKHASH", True)
 
+    conn = d.getVar("__PRSERV_CONN", True)
+    if conn is None:
+        conn = oe.prservice.prserv_make_conn(d)
+        if conn is None:
+            return None
+
+    if "AUTOINC" in pv:
+        srcpv = bb.fetch2.get_srcrev(d)
+        base_ver = "AUTOINC-%s" % version[:version.find(srcpv)]
+        value = conn.getPR(base_ver, pkgarch, srcpv)
+        d.setVar("PKGV", pv.replace("AUTOINC", str(value)))
+
     if d.getVar('PRSERV_LOCKDOWN', True):
         auto_rev = d.getVar('PRAUTO_' + version + '_' + pkgarch, True) or d.getVar('PRAUTO_' + version, True) or None
     else:
-        conn = d.getVar("__PRSERV_CONN", True)
-        if conn is None:
-            conn = oe.prservice.prserv_make_conn(d)
-            if conn is None:
-                return None
         auto_rev = conn.getPR(version, pkgarch, checksum)
 
     return auto_rev

thaugustin/elito-org.openembedded.core

commit 222f83823a11f1f058dcc46d9490ec8887938d97

package.bbclass: replace AUTOINC directly in PKGV
b27b438221e16ac3df6ac66d761b77e3bd43db67 introduced a regression when
PKGV is modified by the recipe.  E.g. when having something like

| PV = "1.7.33+gitr${SRCPV}"
| PKGV = "1.7.33+gitr${GITPKGV}"
| RDEPENDS_${PN}-subpkg += "${PN} (= ${EXTENDPKGV})"
| inherit gitpkgv

The resulting core package has

| Version: 1.7.33+git234+cd01e8c-r11

and because the subpackage has

| Depends: vdr (= 1.7.33+git0+cd01e8c18d494150730214f033f84e5282ce7639-r11)

image build fails with

|  * satisfy_dependencies_for: Cannot satisfy the following dependencies ...
|  *    vdr (= 1.7.33+git0+cd01e8c18d494150730214f033f84e5282ce7639-r11)

Patch avoids the 'PKGV = "cleanup(${PV})"' operation and replaces
AUTOINC directly within PKGV.  This might introduce a regression too
when 'AUTOINC' is added to PKGV intentionally but impact is small and
can be fixed trivially by using another string.

Signed-off-by: Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
Cc: Constantin Musca <constantinx.musca@intel.com>

meta/classes/package.bbclass
@@ -418,9 +418,9 @@ python package_get_auto_pr() {
             return
         d.setVar('PRAUTO',str(auto_pr))
     else:
-        pv = d.getVar("PV", True)
-        if 'AUTOINC' in pv:
-            d.setVar("PKGV", pv.replace("AUTOINC", "0"))
+        pkgv = d.getVar("PKGV", True)
+        if 'AUTOINC' in pkgv:
+            d.setVar("PKGV", pkgv.replace("AUTOINC", "0"))
 }
 
 LOCALEBASEPN ??= "${PN}"